7eb03385c5d7ae32a47de4fe5ff4caaa26e977ce
YueLoung <ss90607@gmail.com>
2020-05-24 13:32:31 +0800

    add file

---
+++ sudoku/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.mySeparator5 = new sudoku.MySeparator();
+            this.mySeparator4 = new sudoku.MySeparator();
+            this.mySeparator16 = new sudoku.MySeparator();
+            this.mySeparator14 = new sudoku.MySeparator();
+            this.SpendTimeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ClearBtn.Location = new System.Drawing.Point(785, 141);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(119, 51);
+            this.ClearBtn.TabIndex = 98;
+            this.ClearBtn.Text = "清空";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // CalculateBtn
+            // 
+            this.CalculateBtn.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.CalculateBtn.Location = new System.Drawing.Point(785, 242);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(119, 48);
+            this.CalculateBtn.TabIndex = 99;
+            this.CalculateBtn.Text = "計算";
+            this.CalculateBtn.UseVisualStyleBackColor = true;
+            this.CalculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
+            // 
+            // mySeparator5
+            // 
+            this.mySeparator5.isVertical = false;
+            this.mySeparator5.Location = new System.Drawing.Point(27, 410);
+            this.mySeparator5.Name = "mySeparator5";
+            this.mySeparator5.Size = new System.Drawing.Size(707, 10);
+            this.mySeparator5.TabIndex = 52;
+            this.mySeparator5.Text = "mySeparator5";
+            this.mySeparator5.Thickness = 4;
+            // 
+            // mySeparator4
+            // 
+            this.mySeparator4.isVertical = false;
+            this.mySeparator4.Location = new System.Drawing.Point(27, 217);
+            this.mySeparator4.Name = "mySeparator4";
+            this.mySeparator4.Size = new System.Drawing.Size(707, 10);
+            this.mySeparator4.TabIndex = 22;
+            this.mySeparator4.Text = "mySeparator4";
+            this.mySeparator4.Thickness = 4;
+            // 
+            // mySeparator16
+            // 
+            this.mySeparator16.isVertical = true;
+            this.mySeparator16.Location = new System.Drawing.Point(242, 26);
+            this.mySeparator16.Name = "mySeparator16";
+            this.mySeparator16.Size = new System.Drawing.Size(10, 593);
+            this.mySeparator16.TabIndex = 97;
+            this.mySeparator16.Text = "mySeparator16";
+            this.mySeparator16.Thickness = 4;
+            // 
+            // mySeparator14
+            // 
+            this.mySeparator14.isVertical = true;
+            this.mySeparator14.Location = new System.Drawing.Point(471, 26);
+            this.mySeparator14.Name = "mySeparator14";
+            this.mySeparator14.Size = new System.Drawing.Size(10, 593);
+            this.mySeparator14.TabIndex = 95;
+            this.mySeparator14.Text = "mySeparator14";
+            this.mySeparator14.Thickness = 4;
+            // 
+            // SpendTimeLabel
+            // 
+            this.SpendTimeLabel.AutoSize = true;
+            this.SpendTimeLabel.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.SpendTimeLabel.Location = new System.Drawing.Point(819, 309);
+            this.SpendTimeLabel.Name = "SpendTimeLabel";
+            this.SpendTimeLabel.Size = new System.Drawing.Size(0, 20);
+            this.SpendTimeLabel.TabIndex = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 662);
+            this.Controls.Add(this.SpendTimeLabel);
+            this.Controls.Add(this.CalculateBtn);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.mySeparator5);
+            this.Controls.Add(this.mySeparator4);
+            this.Controls.Add(this.mySeparator16);
+            this.Controls.Add(this.mySeparator14);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "數獨解題";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MySeparator mySeparator4;
+        private MySeparator mySeparator5;
+        private MySeparator mySeparator14;
+        private MySeparator mySeparator16;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.Label SpendTimeLabel;
+    }
+}
+
---
+++ sudoku/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            iniLayout();
+        }
+
+        #region layout and textBox event
+        public void iniLayout()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    TextBox textBox = new TextBox();
+
+                    textBox.Size = new Size(42, 20);
+                    textBox.BackColor = System.Drawing.SystemColors.Menu;
+                    textBox.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+                    textBox.Location = new System.Drawing.Point(22 + (j * 57), 33 + (i * 51));
+                    textBox.MaxLength = 1;
+                    textBox.Name = $"Pos_{i}_{j}";
+                    textBox.TabIndex = 0;
+                    textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+                    textBox.KeyPress += textBox_KeyPress;
+
+                    this.Controls.Add(textBox);
+                }
+            }
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+        #endregion
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            foreach(var c in this.Controls)
+            {
+                if (c is TextBox)
+                    ((TextBox)c).Text = "";
+            }
+        }
+
+        private void CalculateBtn_Click(object sender, EventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            List<List<int>> board = getBoardDataFromUI();
+            List<List<List<int>>> possibelBoard = getPossibleListBoard(board);
+
+            int[] pos = getFirstToFindPos(board);
+            if (pos.Count() == 0) return;
+
+            List<List<int>> result = findAnswer(board, possibelBoard, pos[0], pos[1]);
+            displayResult2toUI(result);
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            SpendTimeLabel.Text = elapsedMs.ToString() + "ms";
+        }
+
+        private void displayResult2toUI(List<List<int>> board)
+        {
+            for (int i = 0; i < board.Count(); i++)
+            {
+                for (int j = 0; j < board[i].Count(); j++)
+                {
+                    ((TextBox)this.Controls.Find($"Pos_{i}_{j}", true)[0]).Text = board[i][j].ToString();
+                }
+            }
+        }
+
+        int[] getFirstToFindPos(List<List<int>> board)
+        {
+            int[] pos = new int[2];
+
+            for (int i = 0; i < board.Count(); i++)
+            {
+                for (int j = 0; j < board[i].Count(); j++)
+                {
+                    if (board[i][j] == 0)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+
+            return pos;
+        }
+
+        private List<List<int>> getBoardDataFromUI()
+        {
+            //init board(those number for test)
+            List<List<int>> board = new List<List<int>>() { new List<int> { 0, 2, 6, 0, 0, 0, 3, 7, 8 },
+                                                            new List<int> { 0, 5, 8, 6, 3, 7, 4, 0, 0 },
+                                                            new List<int> { 0, 4, 7, 0, 0, 0, 5, 6, 1 },
+                                                            new List<int> { 0, 0, 0, 7, 2, 0, 9, 0, 0 },
+                                                            new List<int> { 0, 0, 0, 3, 0, 8, 2, 5, 0 },
+                                                            new List<int> { 8, 0, 2, 0, 0, 0, 0, 1, 0 },
+                                                            new List<int> { 4, 6, 9, 5, 0, 1, 0, 0, 0 },
+                                                            new List<int> { 0, 0, 1, 9, 0, 0, 7, 4, 0 },
+                                                            new List<int> { 0, 3, 0, 0, 4, 0, 0, 9, 0 }};
+
+            foreach (var c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+                    string[] str = ((TextBox)c).Name.Split('_');
+
+                    int val = 0;
+                    bool isParsable = Int32.TryParse(((TextBox)c).Text, out val);
+
+                    board[Convert.ToInt32(str[1])][Convert.ToInt32(str[2])] = val;
+                }
+            }
+
+            return board;
+        }
+
+        private List<List<List<int>>> getPossibleListBoard(List<List<int>> board)
+        {
+            List<List<List<int>>> res = new List<List<List<int>>>();
+
+            for (int i = 0; i < board.Count(); i++)
+            {
+                List<List<int>> rowList = new List<List<int>>();
+
+                for (int j = 0; j < board[i].Count(); j++)
+                {
+                    if (board[i][j] == 0)
+                    {
+                        List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+                        for (int k = 0; k < board[i].Count(); k++)
+                        {
+                            list.Remove(board[i][k]);
+                        }
+
+                        for (int k = 0; k < board.Count(); k++)
+                        {
+                            list.Remove(board[k][j]);
+                        }
+
+                        int x = (i / 3) * 3;
+                        int y = (j / 3) * 3;
+                        for (int k = 0; k < 9; k++)
+                        {
+                            list.Remove(board[x + k / 3][y + k % 3]);
+                        }
+  
+                        //if (list.Count() == 1)
+                        //{
+                        //    board[i][j] = list[0];
+                        //    list.RemoveAt(0);
+                        //    i = 0;
+                        //    j = 0;
+                        //    res.Clear();
+                        //    break;
+                        //}
+
+                        rowList.Add(list);
+                    }
+                    else
+                    {
+                        rowList.Add(new List<int>());
+                    }
+                }
+
+                res.Add(rowList);
+            }
+
+            return res;
+        }
+
+        private List<List<int>> findAnswer(List<List<int>> board, List<List<List<int>>> possibleTable, int depth_y, int depth_x)
+        {
+            int a = 0;
+            if (depth_y == 8 && depth_x == 8)
+                a= 2;
+
+            List<List<int>> copyBoard = getCopyBoard(board);
+
+            List<List<int>> res = new List<List<int>>();
+
+            if (depth_y == 9)
+                return copyBoard;
+
+            foreach (int val in possibleTable[depth_y][depth_x])
+            {
+                copyBoard[depth_y][depth_x] = val;
+
+                if (isLegal(copyBoard, depth_y, depth_x))
+                {
+                    int next_x = depth_x;
+                    int next_y = depth_y;
+
+                    ++next_x;
+
+                    if (next_x >= 9)
+                    {
+                        next_x = 0;
+                        next_y++;
+                    }
+
+                    while (next_y < 9 && possibleTable[next_y][next_x].Count == 0)
+                    {
+                        ++next_x;
+
+                        if (next_x >= 9)
+                        {
+                            next_x = 0;
+                            next_y++;
+                        }
+                    }
+
+                    var tmp = findAnswer(copyBoard, possibleTable, next_y, next_x);
+
+                    if (tmp.Capacity != 0)
+                    {
+                        res = tmp;
+                        return res;
+                    }
+                        
+                }
+            }
+
+            return res;
+        }
+
+        private List<List<int>> getCopyBoard(List<List<int>> board)
+        {
+            List<List<int>> copyBoard = new List<List<int>>();
+            for (int i = 0; i < board.Count(); i++)
+            {
+                List<int> rowList = new List<int>(board[i]);
+                copyBoard.Add(rowList);
+            }
+
+            return copyBoard;
+        }
+
+        private bool isLegal(List<List<int>> board, int y, int x)
+        {
+            int val = board[y][x];
+
+            for (int k = 0; k < board[0].Count(); k++)
+            {
+                if (k == x)
+                    continue;
+
+                if (val == board[y][k])
+                    return false;
+            }
+
+            for (int k = 0; k < board.Count(); k++)
+            {
+                if (k == y)
+                    continue;
+
+                if (val == board[k][x])
+                    return false;
+            }
+
+            int ini_y = (y / 3) * 3;
+            int ini_x = (x / 3) * 3;
+
+            for (int k = 0; k < board.Count(); k++)
+            {
+                if (ini_y + k / 3 == y && ini_x + k % 3 == x)
+                    continue;
+
+                if (val == board[ini_y + k / 3][ini_x + k % 3])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
